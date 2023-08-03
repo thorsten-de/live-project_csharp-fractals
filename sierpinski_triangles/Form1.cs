5ef146b14f5f09eca844a9e5761c8ec3ca3707af
Thorsten Deienrt <thorsten.deinert@udo.edu>
2023-08-03 20:58:50 +0200

    Draw Sierpinsky triangles

--- sierpinski_triangles/Form1.cs
+++ sierpinski_triangles/Form1.cs
@@ -38,32 +38,41 @@ namespace SierpinskyTriangles
             g.SmoothingMode = SmoothingMode.AntiAlias;
             g.Clear(curvePictureBox.BackColor);
 
-
             DrawSiepinskyTriangle(g, depth, new PointF(width / 2f, 10), new PointF(10, height - 10), new PointF(width - 10, height - 10));
-
 
             curvePictureBox.Image = bitmap;
         }
 
         private void DrawSiepinskyTriangle(Graphics g, int depth, PointF p1, PointF p2, PointF p3)
         {
-            g.FillPolygon(Brushes.Red, new PointF[] { p1, p2, p3 });
+            if (depth < 1)
+            {
+                g.FillPolygon(Brushes.Red, new PointF[] { p1, p2, p3 });
+                return;
+            }
+
+            PointF half12 = Interpolate(p1, p2);
+            PointF half13 = Interpolate(p1, p3);
+            PointF half23 = Interpolate(p2, p3);
+
+            DrawSiepinskyTriangle(g, depth - 1, p1, half12, half13);
+            DrawSiepinskyTriangle(g, depth - 1, half12, p2, half23);
+            DrawSiepinskyTriangle(g, depth - 1, half13, half23, p3);
         }
+
+        private PointF Interpolate(PointF p1, PointF p2, float ratio = 0.5f) =>
+            new PointF(
+                Interpolate(p1.X, p2.X, ratio),
+                Interpolate(p1.Y, p2.Y, ratio));
+
+        private float Interpolate(float f1, float f2, float ratio = 0.5f) => (f1 * ratio) + (f2 * (1 - ratio));
 
         private T ConvertInput<T>(TextBox textBox, Func<string, T> converter) =>
             converter(textBox.Text);
 
-
         private void ShowValidationError(string errorMessage)
         {
             MessageBox.Show(errorMessage, "Draw tree fractal", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-        }
-
-        private void DrawRelative(Graphics g, SizeF delta, ref PointF pos)
-        {
-            PointF destination = PointF.Add(pos, delta);
-            g.DrawLine(Pen, pos, destination);
-            pos = destination;
         }
     }
 }
