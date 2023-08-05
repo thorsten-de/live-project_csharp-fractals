486b517ba6c4db110e5f071be93d23dc3afc7ca1
Thorsten Deienrt <thorsten.deinert@udo.edu>
2023-08-05 19:14:14 +0200

    Initialize form variables

--- ChaosGame/Form1.Designer.cs
+++ ChaosGame/Form1.Designer.cs
@@ -28,18 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            toolStrip1 = new ToolStrip();
+            startButton = new ToolStripButton();
+            chaosPictureBox = new PictureBox();
+            dotsTimer = new System.Windows.Forms.Timer(components);
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)chaosPictureBox).BeginInit();
             SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(24, 24);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { startButton });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 34);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // startButton
+            // 
+            startButton.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            startButton.Image = (Image)resources.GetObject("startButton.Image");
+            startButton.ImageTransparentColor = Color.Magenta;
+            startButton.Name = "startButton";
+            startButton.Size = new Size(57, 29);
+            startButton.Text = "Start!";
+            // 
+            // chaosPictureBox
+            // 
+            chaosPictureBox.Dock = DockStyle.Fill;
+            chaosPictureBox.Location = new Point(0, 34);
+            chaosPictureBox.Name = "chaosPictureBox";
+            chaosPictureBox.Size = new Size(800, 416);
+            chaosPictureBox.TabIndex = 1;
+            chaosPictureBox.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(chaosPictureBox);
+            Controls.Add(toolStrip1);
             Name = "Form1";
             Text = "Form1";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)chaosPictureBox).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton startButton;
+        private PictureBox chaosPictureBox;
+        private System.Windows.Forms.Timer dotsTimer;
     }
 }
--- ChaosGame/Form1.cs
+++ ChaosGame/Form1.cs
@@ -2,6 +2,16 @@ namespace ChaosGame
 {
     public partial class Form1 : Form
     {
+
+        private bool Drawing = false;
+        private Random rand = new Random();
+        private int NumDotsDrawn = 0;
+
+        private Bitmap ChaosBitmap = null!;
+
+        private PointF[] Points = null!;
+        private PointF? CurrentPoint;
+
         public Form1()
         {
             InitializeComponent();
